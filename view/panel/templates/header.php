7289f66ce3dcc1a668e6a6dc60b8788610782c9a
LukiPuki4 <50864177+LukiPuki4@users.noreply.github.com>
2019-05-21 13:36:39 +0200

    Add files via upload

--- view/panel/templates/header.php
+++ view/panel/templates/header.php
@@ -1,4 +1,14 @@
-Głowa<br>
-<title><?= $title; ?></title>
-----
-<br><br>
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<!-- Basic Page Needs
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+		<meta charset="utf-8">
+		<title>Tytuł</title>
+		<meta name="description" content="..">
+		<meta name="author" content="..">
+		
+		<!-- Mobile Specific Metas
+		–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
--- view/panel/test.php
+++ view/panel/test.php
@@ -1,6 +1,7 @@
-KAMILSO
-<?PHP echo $zmienna;
-echo '<br>';
-echo $servers['normalCount'];
-echo $servers['pendingCount'];
-?>
+<body>
+	<?PHP echo $zmienna;
+	echo '<br>';
+	echo $servers['normalCount'];
+	echo $servers['pendingCount'];
+	?>
+</body>
