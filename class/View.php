e77a95b3abc6e7e679cd572289e2e7fae20dca70
LukiPuki4 <50864177+LukiPuki4@users.noreply.github.com>
2019-05-21 10:22:25 +0200

    Add files via upload

--- class/View.php
+++ class/View.php
@@ -1,26 +1,26 @@
 <?PHP
 	class View
-	{
-		public function render(array $data = [])
-		{
-			extract($data);
-			
-			if (is_readable('view/panel/templates/header.php')) {
-				require_once 'view/panel/templates/header.php';
-			} else {
-				die('Wystąpił błąd: header not found.');
-			}
-			
-			if (is_readable("view/panel/{$page}.php")) {
-				require_once "view/panel/{$page}.php";
-			} else {
-				die('Nie znaleziono strony.');
-			}
-			
-			if (is_readable('view/panel/templates/footer.php')) {
-				require_once 'view/panel/templates/footer.php';
-			} else {
-				die('Wystąpił błąd: footer not found.');
-			}
-		}
-	}
+    {
+        public function render(array $data = [])
+        {
+            extract($data);
+
+            if (is_readable('view/panel/templates/header.php')) {
+                require_once 'view/panel/templates/header.php';
+            } else {
+                die('Header not found.');
+            }
+
+            if (is_readable("view/panel/{$page}.php")) {
+                require_once "view/panel/{$page}.php";
+            } else {
+                die("{$page}.php not found.");
+            }
+
+            if (is_readable('view/panel/templates/footer.php')) {
+                require_once 'view/panel/templates/footer.php';
+            } else {
+                die('Footer not found.');
+            }
+        }
+    }
